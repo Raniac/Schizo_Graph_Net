d6c90b15b111c08abd5d763955ffb9a9b3acbd34
raniac <leibingye@outlook.com>
2019-12-31 09:37:18 +0800

    Optimization: change initial learning rate to 0.05 and decay by 0.2 per 60 epochs.

--- main.py
+++ main.py
@@ -18,11 +18,12 @@ from torch_geometric.utils import add_self_loops
 from utils.loader import *
 from models import *
 
-# ==== Create dataset with multiple data
+## ==== Create dataset with multiple data
 # train_dataset, test_dataset = fromPickle2Dataset('/workspace/schizo_graph_net/data/bennyray_191107_347_bcn.pkl')
 # train_dataset, test_dataset = fromPickle2DatasetWithFeature('/workspace/schizo_graph_net/data/bennyray_191107_347_bcn.pkl', '/workspace/schizo_graph_net/data/RANIAC_181210_345_sfMRI_90.csv')
 # train_dataset, test_dataset = fromTxt2Dataset('/workspace/schizo_graph_net/data/ByDPABI/')
 train_dataset, test_dataset = fromTxt2DatasetWithFeature('/workspace/schizo_graph_net/data/test_dpabi/', '/workspace/schizo_graph_net/data/RANIAC_181210_345_sfMRI_90.csv')
+
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
@@ -32,9 +33,9 @@ else:
     logging.info('Using CPU')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Net_191225().to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-1)
-# TODO add learning-rate scheduler.
-scheduler = lr_scheduler.StepLR(optimizer, step_size=60, gamma=0.1)
+optimizer = torch.optim.Adam(model.parameters(), lr=5e-2)
+## learning-rate scheduler.
+scheduler = lr_scheduler.StepLR(optimizer, step_size=60, gamma=0.2)
 
 def train(data_loader, data_size):
     model.train()
@@ -80,15 +81,21 @@ def test(data_loader, data_size):
     
     return test_loss, test_acc, test_out
 
-for epoch in range(1, 241):
+for epoch in range(1, 361):
     scheduler.step()
     train_loss, train_acc = train(train_loader, len(train_dataset))
     test_loss, test_acc, _ = test(test_loader, len(test_dataset))
     logging.info('Epoch {:03d}, Train Loss: {:.4f}, Train Accuracy: {:.4f}, Test Loss: {:.4f}, Test Accuracy: {:.4f}'.format(epoch, train_loss, train_acc, test_loss, test_acc))
 
 test_loss, test_acc, test_out = test(test_loader, len(test_dataset))
+test_check = []
 for idx in range(len(test_out[0])):
     test_out[0][idx] = test_out[0][idx].item()
     test_out[1][idx] = test_out[1][idx].item()
+    if test_out[0][idx] == test_out[1][idx]:
+        test_check.append(1)
+    else:
+        test_check.append(0)
 print(test_out[0])
 print(test_out[1])
+print(test_check)

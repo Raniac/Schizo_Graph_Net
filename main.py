ceb891eab7be175efa7275b58cdc184d7773706e
raniac <leibingye@outlook.com>
2020-01-01 20:13:26 +0800

    Set random state to ensure reproducibility.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ hdlr = logging.FileHandler('logs/train_val.log')
 # hdlr = logging.FileHandler('logs/train_val_' + time.strftime('%Y-%m-%d-%H-%M-%S') + '.log')
 hdlr.setFormatter(logging.Formatter('[%(asctime)s %(levelname)s] %(message)s'))
 logger.addHandler(hdlr)
+import random
+import numpy as np
 
 import torch
 import torch.nn.functional as F
@@ -18,14 +20,28 @@ from torch_geometric.utils import add_self_loops
 from utils.loader import *
 from models import *
 
-## ==== Create dataset with multiple data
+## Hyper-parameter setting
+SEED          = 1
+BATCH_SIZE    = 32
+LEARNING_RATE = 5e-2
+LR_STEP_SIZE  = 60
+LR_GAMMA      = 0.2
+NUM_EPOCHS    = 200
+
+## Ensure reproducibility
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed_all(SEED)
+
+## Create dataset with multiple data
 # train_dataset, test_dataset = fromPickle2Dataset('/workspace/schizo_graph_net/data/bennyray_191107_347_bcn.pkl')
 # train_dataset, test_dataset = fromPickle2DatasetWithFeature('/workspace/schizo_graph_net/data/bennyray_191107_347_bcn.pkl', '/workspace/schizo_graph_net/data/RANIAC_181210_345_sfMRI_90.csv')
 # train_dataset, test_dataset = fromTxt2Dataset('/workspace/schizo_graph_net/data/ByDPABI/')
 train_dataset, test_dataset = fromTxt2DatasetWithFeature('/workspace/schizo_graph_net/data/test_dpabi/', '/workspace/schizo_graph_net/data/RANIAC_181210_345_sfMRI_90.csv')
 
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
 if torch.cuda.is_available():
     logging.info('Using GPU')
@@ -33,9 +49,9 @@ else:
     logging.info('Using CPU')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Net_191225().to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=5e-2)
+optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 ## learning-rate scheduler.
-scheduler = lr_scheduler.StepLR(optimizer, step_size=60, gamma=0.2)
+scheduler = lr_scheduler.StepLR(optimizer, step_size=LR_STEP_SIZE, gamma=LR_GAMMA)
 
 def train(data_loader, data_size):
     model.train()
@@ -81,7 +97,7 @@ def test(data_loader, data_size):
     
     return test_loss, test_acc, test_out
 
-for epoch in range(1, 361):
+for epoch in range(1, NUM_EPOCHS+1):
     scheduler.step()
     train_loss, train_acc = train(train_loader, len(train_dataset))
     test_loss, test_acc, _ = test(test_loader, len(test_dataset))
